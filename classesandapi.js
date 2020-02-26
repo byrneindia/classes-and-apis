fa2b78f3b556344e2fb37f34f9b56b014334e5e9
India Byrne <byrneindia@gmail.com>
2020-02-26 15:17:36 -0700

    VCS upload.

---
+++ classesandapi.js
@@ -0,0 +1,15 @@
+/*
+* Objects, Interfaces, and APIs
+ */
+
+let dog ={
+	name:"Steve",
+	color: "brown",
+	breed: "Pug",
+	size: "Small",
+	bark: function (typeOfBark){
+		console.log ("Bark!")
+	},
+};
+
+dog.bark();
